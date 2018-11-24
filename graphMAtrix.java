4c97556f6da79673867fa826f58dd402e1acf3bb
kunalpahwa <kunalpahwa0001@gmail.com>
2018-11-24 10:31:41 +0530

    update

--- graphMAtrix.java
+++ graphMAtrix.java
@@ -1,17 +1,6 @@
-/* IMPORTANT: Multiple classes and nested static classes are supported */
-
-
-// * uncomment this if you want to read input.
-//imports for BufferedReader
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-//import for Scanner and other utility classes
 import java.io.*;
 import java.util.*;
 
-
-// Warning: Printing unwanted or ill-formatted data to output will cause the test cases to fail
 class Graph{
     private int V;
     public LinkedList<Integer> adj[];
@@ -39,22 +28,7 @@ class Graph{
 }
 class TestClass {
     public static void main(String args[] ) throws Exception {
-        /* Sample code to perform I/O:
-         * Use either of these methods for input
-
-        //BufferedReader
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String name = br.readLine();                // Reading input from STDIN
-        System.out.println("Hi, " + name + ".");    // Writing output to STDOUT
-
-        //Scanner
-        Scanner s = new Scanner(System.in);
-        String name = s.nextLine();                 // Reading input from STDIN
-        System.out.println("Hi, " + name + ".");    // Writing output to STDOUT
-
-        */
-
-        // Write your code here
+      
         Scanner sc = new Scanner(System.in);
         int nodes = sc.nextInt();
         int edges = sc.nextInt();
